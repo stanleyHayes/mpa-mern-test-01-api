803e9cfc2038e47c92cc2f779f8779fa49ff5808
Stanley Hayford <hayfordstanley@gmail.com>
2021-04-11 07:57:41 +0000

    get user by email, update user by email controller implemented

--- index.js
+++ index.js
@@ -41,18 +41,39 @@ app.post('/api/v1/users', async (req, res) => {
 });
 
 
-app.get('/api/v1/users', async (req, res) => {
+app.put('/api/v1/users/:email', async (req, res) => {
     try {
-
+        const {email, name, bio, occupation, languages, github} = req.body;
+        if (!req.params.email) {
+            return res.status(400).json({message: `email required`});
+        }
+        let user = await User.findOne({email});
+        if (!user) {
+            user = await User.create({email, name, occupation, bio, languages, github});
+            return res.status(201).json({message: `User details successfully saved`, data: user});
+        }
+        for (let key of Object.keys(req.body)) {
+            user[key] = req.body[key];
+        }
+        await user.save();
+        res.status(200).json({data: user});
     } catch (e) {
         res.status(500).json({message: `${e.message}`});
     }
 });
 
 
-app.get('/api/v1/users/:id', async (req, res) => {
+app.get('/api/v1/users/:email', async (req, res) => {
     try {
-
+        const email = req.params.email;
+        if (!email) {
+            return res.status(400).json({message: `email required`});
+        }
+        const user = await User.findOne({email});
+        if (!user) {
+            return res.status(404).json({message: `user with email ${email} not found`});
+        }
+        res.status(200).json({data: user});
     } catch (e) {
         res.status(500).json({message: `${e.message}`});
     }
